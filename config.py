141623f621263ae0f30c2336875736cfac65c2d5
Mothis <andrey59995@gmail.com>
2021-05-05 13:04:09 +0300

    edited config.py - database_url

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 
 class TestConfig(Config):
